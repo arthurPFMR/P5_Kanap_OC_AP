866afa3d88accadc7c0772985c1e743d6c57fcb6
Arthur <arthur.pulfermuller@gmail.com>
2022-09-15 15:34:00 +0200

    amelioration-du -storage

---
+++ front/js/cart.js
@@ -0,0 +1,9 @@
+let productToLocalStorage = JSON.parse(localStorage.getItem("#addToCart"));
+
+const cartBasket = document.querySelector("#cart__items");
+if (productToLocalStorage === null) {
+  alert("Le panier est vide");
+} else {
+}
+
+
